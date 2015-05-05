ca4c35daf8bae248ac78b1d6a9ab2f5936216c77
Andrew Larose <werdnalarose@gmail.com>
2015-05-05 11:36:03 -0400

    initial commit

---
+++ app/components/pitch.js
@@ -0,0 +1,1 @@
+/** @jsx REact.DOM */
---
+++ index.js
@@ -0,0 +1,7 @@
+# add more custom features availabe through params
+
+# dimension param(s) in api url
+# use to set width and height
+
+# use react to render svg
+# React.renderToString
